af7030e92f14411c2a4bae970f3950dcfcf199be
huelton <hueltondihon@gmail.com>
2021-05-07 08:40:47 -0300

    home,navigation

--- frontend/src/components/datatable/index.tsx
+++ frontend/src/components/datatable/index.tsx
@@ -3,41 +3,41 @@ import React from "react";
 const  DataTable = () => {
   return (
     <div className="table-responsive">
-    <table className="table table-striped table-sm">
-        <thead>
-            <tr>
-                <th>Data</th>
-                <th>Vendedor</th>
-                <th>Clientes visitados</th>
-                <th>Negócios fechados</th>
-                <th>Valor</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>23/04/2021</td>
-                <td>Josh</td>
-                <td>34</td>
-                <td>25</td>
-                <td>14976.00</td>
-            </tr>
-            <tr>
-                <td>24/04/2021</td>
-                <td>Allen Michel</td>
-                <td>34</td>
-                <td>25</td>
-                <td>12983.00</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+        <table className="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Vendedor</th>
+                    <th>Clientes visitados</th>
+                    <th>Negócios fechados</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Barry Allen</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>23/04/2021</td>
+                    <td>Josh</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>14976.00</td>
+                </tr>
+                <tr>
+                    <td>24/04/2021</td>
+                    <td>Allen Michel</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>12983.00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
   );
 }
 
